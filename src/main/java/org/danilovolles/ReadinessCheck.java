0fbb6a0cd4543ad407e55612fcd888e2e57242e6
Danilo Volles <danilo.volles@outlook.com>
2026-02-22 23:08:05 -0300

    implementação de service e implementação do liveness e readiness check

---
+++ src/main/java/org/danilovolles/ReadinessCheck.java
@@ -0,0 +1,23 @@
+package org.danilovolles;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Readiness;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+@Readiness
+public class ReadinessCheck implements HealthCheck {
+
+    @RestClient
+    StarWarsService starWarsService;
+
+    @Override
+    public HealthCheckResponse call() {
+
+        if (starWarsService.getStarships().contains(StarWarsService.MSG_ERROR)){
+            return HealthCheckResponse.down("NÃO ESTOU PRONTA");
+        }
+
+        return HealthCheckResponse.up("ESTOU PRONTA");
+    }
+}
--- src/main/java/org/danilovolles/StarWarsService.java
+++ src/main/java/org/danilovolles/StarWarsService.java
@@ -4,13 +4,32 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import java.time.temporal.ChronoUnit;
 
 @RegisterRestClient(baseUri = "https://swapi.info/api/")
 public interface StarWarsService {
 
+    String MSG_ERROR = "Fallback ";
+
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     @Path("starships")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Timeout(value = 3000L)
+    @Fallback(fallbackMethod = "starshipsFallback")
+    @CircuitBreaker(
+            requestVolumeThreshold = 2, // Avalia o estado após pelo menos 2 chamadas.
+            failureRatio = .5,          // Se 50% ou mais dessas chamadas falharem, o circuito abre.
+            delay = 3000L,              // Após abrir, espera 3s antes de testar novamente.
+            successThreshold = 2        // Precisa de 2 sucessos seguidos para fechar o circuito.
+    )
     String getStarships();
+
+    default String starshipsFallback() {
+        return MSG_ERROR;
+    }
 }
